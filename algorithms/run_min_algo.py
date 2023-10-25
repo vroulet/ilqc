added additional measures: rate of convergence and minimal singular value of the jacobian, fixed dependency of pyglet (backward incompatible changes on their end

algorithms/run_min_algo.py
@@ -1,17 +1,18 @@
 import torch
 import math
 import time
-from typing import List
+from typing import List, Tuple
 
 from envs.forward import DiffEnv
 from algorithms.algos_steps import min_step
 from utils_pipeline.optim_helpers import print_info_step
+from envs.torch_utils import auto_multi_grad
 
 
 def run_min_algo(env: DiffEnv, max_iter: int = 10, algo: str = 'ddp_linquad_reg', stepsize: float = None,
-                 line_search: bool = True, handle_bad_dir: str = 'flag',
+                 line_search: bool = True, handle_bad_dir: str = 'flag', compute_min_sev: bool = False,
                  prev_cmd: torch.Tensor = None, optim_aux_vars: dict = None,  past_metrics: dict = None)\
-                 -> (torch.Tensor, dict, dict):
+                 -> Tuple[torch.Tensor, dict, dict]:
     """
     Run a given nonlinear control algorithm for a given nonlinear control environment, outputs a candidate optimal
     sequence of controls, auxiliary variables to restart the optimization process and some metrics about the
@@ -35,7 +36,7 @@ def run_min_algo(env: DiffEnv, max_iter: int = 10, algo: str = 'ddp_linquad_reg'
     cmd = prev_cmd if prev_cmd is not None else torch.zeros(env.horizon, env.dim_ctrl)
     cmd.requires_grad = True
 
-    algo_type, approx, step_mode = check_nomenclature(algo)
+    _, approx, step_mode = check_nomenclature(algo)
 
     default_stepsize = 100. if 'reg' in step_mode else 1.
     if optim_aux_vars is not None:
@@ -45,7 +46,7 @@ def run_min_algo(env: DiffEnv, max_iter: int = 10, algo: str = 'ddp_linquad_reg'
 
     if past_metrics is None:
         traj, costs = env.forward(cmd, approx=approx)
-        metrics, iteration = collect_info(None, env, cmd, costs, 0, stepsize, algo, 0.), 0
+        metrics, iteration = collect_info(None, env, cmd, costs, 0, stepsize, algo, 0., compute_min_sev), 0
     else:
         traj, costs = env.forward(cmd, approx=approx)
         metrics, iteration = past_metrics, past_metrics['iteration'][-1]
@@ -57,7 +58,7 @@ def run_min_algo(env: DiffEnv, max_iter: int = 10, algo: str = 'ddp_linquad_reg'
                                               line_search, algo, handle_bad_dir)
         iteration += 1
         time_iter = time.time() - start_time
-        metrics = collect_info(metrics, env, cmd, costs, iteration, stepsize, algo, time_iter)
+        metrics = collect_info(metrics, env, cmd, costs, iteration, stepsize, algo, time_iter, compute_min_sev)
         status = check_cvg_status(metrics)
 
     cmd_opt = cmd.data if cmd is not None else None
@@ -65,8 +66,23 @@ def run_min_algo(env: DiffEnv, max_iter: int = 10, algo: str = 'ddp_linquad_reg'
     return cmd_opt, optim_aux_vars, metrics
 
 
+def compute_min_sing_eigval_jac(cmd, env):
+    cmd_shape = cmd.shape
+
+    flat_cmd = cmd.view(-1)
+    unflat_cmd = flat_cmd.view(*cmd_shape)
+    traj, _ = env.forward(unflat_cmd)
+    traj_ = torch.stack(traj[1:])
+    flat_traj = traj_.view(-1)
+
+    jac_t = auto_multi_grad(flat_traj, flat_cmd)
+
+    _, s, _ = torch.svd(jac_t.t().mm(jac_t)) 
+    return min(s).item()
+
+
 def collect_info(metrics: dict, env: DiffEnv, cmd: torch.Tensor, costs: List[torch.Tensor], iteration: int,
-                 stepsize: float, algo: str, time_iter: float) -> dict:
+                 stepsize: float, algo: str, time_iter: float, compute_min_sev: bool) -> dict:
     """
     Collect metric on the problem and add it to the current metrics
     :param metrics: metrics of the optimization process computed so far
@@ -92,9 +108,18 @@ def collect_info(metrics: dict, env: DiffEnv, cmd: torch.Tensor, costs: List[tor
         _, costs = env.forward(cmd)
         norm_grad_obj = torch.norm(torch.autograd.grad(sum(costs), cmd)[0])
 
-    info_step = dict(iteration=iteration, time=cumul_time + time_iter, cost=cost.item(),
-                     norm_grad_obj=norm_grad_obj.item(), stepsize=stepsize, algo=algo)
+    if compute_min_sev:
+      min_sev_jac = compute_min_sing_eigval_jac(cmd, env)
+
+    info_step = dict(iteration=iteration,
+                     time=cumul_time + time_iter,
+                     cost=cost.item(),
+                     norm_grad_obj=norm_grad_obj.item(), 
+                     stepsize=stepsize, algo=algo)
     format_types = ['int', 'time', 'scientific', 'scientific', 'scientific', 'string']
+    if compute_min_sev:
+        info_step.update(min_sev_jac=min_sev_jac)
+        format_types.append['scientific']
     print_info_step(info_step, format_types, iteration == 0)
 
     if metrics is None:
@@ -154,7 +179,7 @@ def check_cost_status(metrics: dict, verbose: bool = True) -> str:
     return status
 
 
-def check_nomenclature(algo: str) -> (str, str, str):
+def check_nomenclature(algo: str) -> Tuple[str, str, str]:
     """
     Check if the algorithm given in the input follows the syntax of the toolbox
     :param algo: given name of the algorithm

envs/backward.py
@@ -1,10 +1,10 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import torch
 
 
 def lin_quad_backward(traj: List[torch.Tensor], costs: List[torch.Tensor],
                       reg_ctrl: float = 0., reg_state: float = 0.,
-                      handle_bad_dir: str = 'flag') -> (List[Dict[str, torch.Tensor]], torch.Tensor, bool):
+                      handle_bad_dir: str = 'flag') -> Tuple[List[Dict[str, torch.Tensor]], torch.Tensor, bool]:
     """
     Compute optimal policies associated to the linear quadratic control problem approximating the environment at the
     current iteration using linear quadratic approximations. See the companion report of the toolbox (
@@ -45,7 +45,7 @@ def lin_quad_backward(traj: List[torch.Tensor], costs: List[torch.Tensor],
 
 def quad_backward(traj: List[torch.Tensor], costs: List[torch.Tensor], reg_ctrl: float = 0., reg_state: float = 0.,
                   handle_bad_dir: str = 'flag', mode: str = 'newton') \
-                  -> (List[Dict[str, torch.Tensor]], torch.Tensor, bool):
+                  -> Tuple[List[Dict[str, torch.Tensor]], torch.Tensor, bool]:
     """
     Compute optimal policies associated to the linear quadratic control problem approximating the environment at the
     current iteration using quadratic approximations. See the companion report of the toolbox (
@@ -105,8 +105,8 @@ def quad_backward_newton(traj, costs, reg_ctrl=0., reg_state=0., handle_bad_dir=
 
 def bell_step(A: torch.Tensor, B: torch.Tensor, J: torch.Tensor, j: torch.Tensor, jcst: torch.Tensor,
               P: torch.Tensor, p: torch.Tensor, Q: torch.Tensor, q: torch.Tensor, R: torch.Tensor = None,
-              handle_bad_dir: str ='flag') -> (torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor,
-                                               bool):
+              handle_bad_dir: str ='flag') -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor,
+                                               bool]:
     r"""
     Compute cost-to-go function associated to the linear quadratic control problem at a given time step. In other
     words, solve the Bellman equation associated to a linear quadratic control problem. Namely, given the cost-to-go
@@ -186,8 +186,8 @@ def bell_core(A, B, J, j, H, P, p, q, R=None):
     """
     if R is None:
         R = torch.zeros(p.shape[0], q.shape[0])
-    K = - torch.solve(R.t() + B.t().mm(J.mm(A)), H)[0]
-    k = - torch.solve((q + B.t().mv(j)).view(-1, 1), H)[0].view(-1)
+    K = - torch.linalg.solve(H, R.t() + B.t().mm(J.mm(A)))
+    k = - torch.linalg.solve(H, (q + B.t().mv(j)))
     next_J = P + A.t().mm(J.mm(A)) + (R + A.t().mm(J.mm(B))).mm(K)
     next_j = p + A.t().mv(j) + A.t().mv(J.mv(B.mv(k))) + R.mv(k)
     rest = 0.5 * (q + B.t().mv(j)).dot(k)

envs/forward.py
@@ -1,6 +1,6 @@
 import torch
 import time
-from typing import Callable, List
+from typing import Callable, List, Tuple
 
 from torch.autograd import grad as auto_grad
 from envs.torch_utils import auto_multi_grad
@@ -32,7 +32,7 @@ class DiffEnv:
         """
         raise NotImplementedError
 
-    def costs(self, next_state: torch.Tensor, ctrl: torch.Tensor, time_iter: int) -> (torch.Tensor, torch.Tensor):
+    def costs(self, next_state: torch.Tensor, ctrl: torch.Tensor, time_iter: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Cost associated to the state that has just been computed and the cost associated to the applied control variable
         To be replaced by the given instance of the problem
@@ -45,7 +45,7 @@ class DiffEnv:
         """
         return torch.tensor(0.), torch.tensor(0.)
 
-    def step(self, ctrl: torch.Tensor, approx: str = None) -> (torch.Tensor, torch.Tensor):
+    def step(self, ctrl: torch.Tensor, approx: str = None) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Step of the environment, uses current state of the environment (treated as an attribute as in gym) and make
         one step.
@@ -75,7 +75,7 @@ class DiffEnv:
         return next_state, cost
 
     def forward(self, cmd: torch.Tensor, approx: str = None, reset: bool = True) \
-            -> (List[torch.Tensor], List[torch.Tensor]):
+            -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
         """
         Compute trajectories and costs associated to a sequence of control variables
         Can record first/second order information on dynamics/costs depending on the input approx (see function step)

finite_horizon_control/compa_algos.py
@@ -6,6 +6,7 @@ from pandas import DataFrame
 from matplotlib import pyplot as plt
 import seaborn as sns
 from copy import deepcopy
+import numpy as np
 
 sys.path.append('..')
 sys.path.append('.')
@@ -17,12 +18,16 @@ from finite_horizon_control.utils_plot import nice_ax, get_nice_writing, set_plt
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 
-def compa_algos_env(env_cfg, optim_cfgs, x_axis, y_axis, plot=False, logscale=False, normalize=True):
+def compa_algos_env(env_cfg, optim_cfgs, x_axis, y_axis, plot=False, logscale=False, normalize=True, revert_rates=False):
     compa_optim = DataFrame()
     for optim_cfg in optim_cfgs:
         exp_outputs = solve_ctrl_pb_incrementally(env_cfg, optim_cfg)
         conv_algo = exp_outputs['metrics']
-        compa_optim = compa_optim.append(DataFrame(conv_algo), ignore_index=True)
+        rates = [conv_algo['cost'][i+1]/conv_algo['cost'][i] for i in range(len(conv_algo['cost']) - 1)] + [1e-12]
+        conv_algo.update({'rate': rates})
+        if revert_rates:
+          conv_algo['rate'] = conv_algo['rate'][::-1]
+        compa_optim = pd.concat([compa_optim, DataFrame(conv_algo)], ignore_index=True)
 
     if y_axis == 'cost':
         min_info = min(compa_optim[y_axis])
@@ -31,11 +36,7 @@ def compa_algos_env(env_cfg, optim_cfgs, x_axis, y_axis, plot=False, logscale=Fa
                    for info in compa_optim[y_axis]]
         else:
             aux = [info / compa_optim[y_axis].to_list()[0] for info in compa_optim[y_axis]]
-        # if env_cfg['env'] == 'cart_pendulum' or env_cfg['env'] == 'simple_car':
-        #     aux = [max(info, 1e-6) for info in aux]
         compa_optim[y_axis] = aux
-    # pd.set_option('display.expand_frame_repr', False, 'display.max_rows', None, 'display.max_columns', None)
-    # print(compa_optim)
 
     if x_axis == 'time':
         mean_final_time = compa_optim.groupby('algo')['time'].max().mean()
@@ -61,7 +62,7 @@ def compa_algos_env_horizons(env_cfg, algos, horizons, max_iter, x_axis, y_axis,
     palette, marker_styles = get_palette_line_styles()
     start_iter_plot, max_iter_plot = 0, max_iter - 5
 
-    fig, axs = plt.subplots(1, len(horizons), squeeze=False, figsize=(16, 5.5))
+    # fig, axs = plt.subplots(1, len(horizons), squeeze=False, figsize=(16, 5.5))
     total_time = total_times[env_cfg['env']]
 
     algos_compared = deepcopy(algos)
@@ -74,30 +75,30 @@ def compa_algos_env_horizons(env_cfg, algos, horizons, max_iter, x_axis, y_axis,
         optim_cfgs = [dict(max_iter=max_iter, algo=algo) for algo in algos_compared]
         compa_optim = compa_algos_env(env_cfg, optim_cfgs, x_axis, y_axis)
 
-        compa_optim = compa_optim[compa_optim['iteration']>=start_iter_plot]
-        compa_optim = compa_optim[compa_optim['iteration']<=max_iter_plot]
+        # compa_optim = compa_optim[compa_optim['iteration']>=start_iter_plot]
+        # compa_optim = compa_optim[compa_optim['iteration']<=max_iter_plot]
 
-        sns.lineplot(x=x_axis, y=y_axis, hue='algo', style='algo',  data=compa_optim, ax=axs[0][j],
-                     markers=marker_styles, dashes=False, palette=palette, markevery=10)
-        if axs[0][j].get_legend() is not None:
-            axs[0][j].get_legend().remove()
-        x_axis = axs[0][j].xaxis.get_label().get_text()
-        axs[0][j].set_xlabel(nice_writing[x_axis])
-        if j == 0:
-            y_axis = axs[0][j].yaxis.get_label().get_text()
-            axs[0][j].set_ylabel(nice_writing[y_axis])
-        else:
-            axs[0][j].set(ylabel=None)
-        axs[0][j].set(yscale='log')
-        axs[0][j].set_title(rf'$\tau={horizon}$')
+        # sns.lineplot(x=x_axis, y=y_axis, hue='algo', style='algo',  data=compa_optim, ax=axs[0][j],
+        #              markers=marker_styles, dashes=False, palette=palette, markevery=10)
+        # if axs[0][j].get_legend() is not None:
+        #     axs[0][j].get_legend().remove()
+        # x_axis = axs[0][j].xaxis.get_label().get_text()
+        # axs[0][j].set_xlabel(nice_writing[x_axis])
+        # if j == 0:
+        #     y_axis = axs[0][j].yaxis.get_label().get_text()
+        #     axs[0][j].set_ylabel(nice_writing[y_axis])
+        # else:
+        #     axs[0][j].set(ylabel=None)
+        # axs[0][j].set(yscale='log')
+        # axs[0][j].set_title(rf'$\tau={horizon}$')
 
-    fig.suptitle(nice_writing[env_cfg['env']], y=1.)
-    fig.tight_layout()
-    if add_legend:
-        handles, labels = axs[0][0].get_legend_handles_labels()
-        labels = [nice_writing[label] for label in labels]
-        fig.legend(handles=handles, labels=labels, loc='center', bbox_to_anchor=(0.5, 0.02), ncol=len(handles))
-
+    # fig.suptitle(nice_writing[env_cfg['env']], y=1.)
+    # fig.tight_layout()
+    # if add_legend:
+    #     handles, labels = axs[0][0].get_legend_handles_labels()
+    #     labels = [nice_writing[label] for label in labels]
+    #     fig.legend(handles=handles, labels=labels, loc='center', bbox_to_anchor=(0.5, 0.02), ncol=len(handles))
+    fig = 0
     return fig
 
 
@@ -162,7 +163,7 @@ def plot_conv_all_algos():
         dict(env='simple_car', track='simple', cost='exact', discretization='euler', reg_bar=0.),
         dict(env='real_car', track='simple', cost='contouring', discretization='rk4_cst_ctrl')
     ]
-    max_iter = 80
+    max_iter = 120
     horizons = [25, 50, 100]
 
     nice_writing = get_nice_writing()
@@ -175,17 +176,14 @@ def plot_conv_all_algos():
                                  for algo_type in ['classic', 'ddp'] for step_mode in ['reg', 'dir']]
 
                 fig = compa_algos_env_horizons(env_cfg, algos, horizons, max_iter, x_axis, y_axis, add_legend=False)
-                if env_cfg['env'] == 'pendulum':
-                    handles, labels = fig.axes[0].get_legend_handles_labels()
-                    labels = [nice_writing[label] for label in labels]
-                if env_cfg['env'] == 'real_car':
-                    fig.legend(handles=handles, labels=labels, loc='center', bbox_to_anchor=(0.5, 0.),
-                               ncol=len(handles))
-                fig.tight_layout()
-                fig.show()
-                # path = '/Users/vincentroulet/git_projects/vincent/ctrl/papers/techreport_arxiv2022/fig' \
-                #         + '/{0}_{1}_{2}_{3}.pdf'.format(env_cfg['env'], approx, x_axis, y_axis)
-                # fig.savefig(path, format='pdf', bbox_inches='tight')
+                # if env_cfg['env'] == 'pendulum':
+                #     handles, labels = fig.axes[0].get_legend_handles_labels()
+                #     labels = [nice_writing[label] for label in labels]
+                # if env_cfg['env'] == 'real_car':
+                #     fig.legend(handles=handles, labels=labels, loc='center', bbox_to_anchor=(0.5, 0.),
+                #                ncol=len(handles))
+                # fig.tight_layout()
+                # fig.show()
 
     env_cfgs = [
         dict(env='pendulum'),
@@ -199,25 +197,22 @@ def plot_conv_all_algos():
         for env_cfg in env_cfgs:
             algos = [algo_type + '_' + approx for algo_type in ['classic', 'ddp']]
             fig = compa_algos_env_stepsizes(env_cfg, algos, horizons, max_iter, x_axis, y_axis, add_legend=False)
-            if env_cfg['env'] == 'pendulum':
-                axs_handles = []
-                axs_labels = []
-                for ax in fig.axes:
-                    handles, labels = ax.get_legend_handles_labels()
-                    for k, label in enumerate(labels):
-                        if nice_writing[label] not in axs_labels:
-                            axs_handles.append(ax.lines[k])
-                            axs_labels.append(nice_writing[labels[k]])
+            # if env_cfg['env'] == 'pendulum':
+            #     axs_handles = []
+            #     axs_labels = []
+            #     for ax in fig.axes:
+            #         handles, labels = ax.get_legend_handles_labels()
+            #         for k, label in enumerate(labels):
+            #             if nice_writing[label] not in axs_labels:
+            #                 axs_handles.append(ax.lines[k])
+            #                 axs_labels.append(nice_writing[labels[k]])
 
-            if env_cfg['env'] == 'simple_car':
-                fig.legend(handles=axs_handles, labels=axs_labels, loc='center', bbox_to_anchor=(0.5, 0.),
-                           ncol=len(axs_handles))
+            # if env_cfg['env'] == 'simple_car':
+            #     fig.legend(handles=axs_handles, labels=axs_labels, loc='center', bbox_to_anchor=(0.5, 0.),
+            #                ncol=len(axs_handles))
 
-            fig.tight_layout()
-            fig.show()
-            # path = '/Users/vincentroulet/git_projects/vincent/ctrl/papers/techreport_arxiv2022/fig' \
-            #        + '/{0}_{1}_{2}_{3}.pdf'.format(env_cfg['env'], approx, x_axis, y_axis)
-            # fig.savefig(path, format='pdf', bbox_inches='tight')
+            # fig.tight_layout()
+            # fig.show()
 
 
 def plot_conv_gd_ilqr_iddp():
@@ -237,12 +232,10 @@ def plot_conv_gd_ilqr_iddp():
     algos = ['gd'] + [algo_type + '_' + approx + '_' + step_mode for algo_type in ['classic', 'ddp']]
 
     fig, axs = plt.subplots(1, len(env_cfgs), figsize=(14, 7))
-    # fig, axs = plt.subplots(2, 1, figsize=(7, 12))
 
     for i, env_cfg in enumerate(env_cfgs):
         max_iter = max_iters[env_cfg['env']]
         optim_cfgs = [dict(max_iter=max_iter, algo=algo) for algo in algos]
-        # fig = plt.figure()
         compa_optim = compa_algos_env(env_cfg, optim_cfgs, x_axis, y_axis, plot=False, logscale=False, normalize=False)
 
         sns.lineplot(x=x_axis, y=y_axis, hue='algo', style='algo', data=compa_optim,
@@ -267,9 +260,7 @@ def plot_conv_gd_ilqr_iddp():
                ncol=len(handles))
     fig.tight_layout()
     fig.show()
-    path = '/Users/vincentroulet/git_projects/vincent/ctrl/papers/ilqc_jmlr2022/fig/simple_conv_example.pdf'
-
-    fig.savefig(path, format='pdf', bbox_inches='tight')
+    fig.savefig('plot_conv_gd_ilqr_iddp.pdf', format='pdf', bbox_inches='tight')
 
 
 def plot_conv_gd_ilqr_ddp_real_car():
@@ -308,19 +299,77 @@ def plot_conv_gd_ilqr_ddp_real_car():
     ax.set_ylabel(nice_writing[y_axis])
 
     ax.set_title(nice_writing[env_cfg['env']])
-    # ax.locator_params(axis="y", numticks=2)
-    # ax._request_autoscale_view(tight=True)
 
     fig.legend(handles=handles, labels=labels, loc='center', bbox_to_anchor=(0.5, 0.),
                ncol=len(handles))
     fig.tight_layout()
     fig.show()
-    path = '/Users/vincentroulet/git_projects/vincent/ctrl/papers/ilqc_jmlr2022/fig/hard_conv_example.pdf'
-    fig.savefig(path, format='pdf', bbox_inches='tight')
+
+
+def plot_rates_and_min_sev():
+  set_plt_params()
+  fig, axs = plt.subplots(1, 2, figsize=(13, 5))
+
+  torch.set_default_dtype(torch.float64)
+
+  env_cfg = dict(env='simple_car', discretization='rk4', cost='exact', reg_bar=0., dt=1.,
+                track='simple', horizon=10)
+  algo = 'ddp_linquad_reg'
+  optim_cfg = dict(algo=algo, max_iter=50)
+  exp_outputs = solve_ctrl_pb_incrementally(env_cfg, optim_cfg)
+
+  metrics = pd.DataFrame(exp_outputs['metrics'])
+
+  nice_writing = get_nice_writing(alternative=True)
+  palette = get_palette_line_styles()[0]
+
+  x_axis, y_axis = 'iteration', 'min_sev_jac'
+  ax = sns.lineplot(x=x_axis, y=y_axis, data=metrics, color=palette[algo], ax=axs[0])
+  ax.set_xlabel(nice_writing[x_axis])
+  ax.set_ylabel(nice_writing[y_axis])
+  ax.set_yscale('log')
+
+  env_cfg = dict(env='simple_car', track='simple', cost='exact', discretization='rk4', reg_bar=0., reg_ctrl=0., horizon=50, dt=2./50)
+  max_iters = dict(pendulum=30, simple_car=500)
+  nice_writing = get_nice_writing(alternative=True)
+  palette, _ = get_palette_line_styles()
+
+  x_axis, y_axis = 'iteration', 'rate'
+  algo = 'ddp_linquad_reg'
+
+  max_iter = max_iters[env_cfg['env']]
+  optim_cfgs = [dict(max_iter=max_iter, algo=algo)]
+  compa_optim = compa_algos_env(env_cfg, optim_cfgs, x_axis, y_axis, plot=False, logscale=False, normalize=False, revert_rates=True)
+
+  compa_optim = compa_optim[compa_optim['iteration']>2]
+  ax = sns.lineplot(x=x_axis, y=y_axis, hue='algo', style='algo', data=compa_optim,
+              dashes=False, palette=palette, markevery=int(max_iter/15), ax=axs[1])
+
+  ax.set(yscale='log')
+  plt.gca().invert_xaxis()
+  ax.set(xscale='log')
+  
+  ax.set_xticks([3., 10., 30., 80.])
+  ax.set_xticklabels(['79', '72', '50', '1'])
+
+  handles, labels = ax.get_legend_handles_labels()
+  labels = [nice_writing[label] for label in labels]
+  ax.get_legend().remove()
+
+  x_axis = ax.xaxis.get_label().get_text()
+  ax.set_xlabel(nice_writing[x_axis])
+  y_axis = ax.yaxis.get_label().get_text()
+  ax.set_ylabel(nice_writing[y_axis])
+
+  fig.legend(handles=handles, labels=labels, loc='center', bbox_to_anchor=(0.5, 0.),
+             ncol=len(handles))
+  
+  fig.suptitle('Simple Model of a Car with Tracking Costs', size='medium', x=0.52, y=0.95)
+  fig.tight_layout()
+  return fig
 
 
 if __name__ == '__main__':
-    # plot_conv_all_algos()
-    plot_conv_gd_ilqr_iddp()
-    # plot_conv_gd_ilqr_ddp_real_car()
+    plot_conv_all_algos()
+    # plot_rates_and_min_sev()
 